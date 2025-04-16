dcbc51a22f84bc9132c4c46312aad0b488cbc975
Erik Rahtjen <erik.rahtjen@gmail.com>
2025-04-16 10:06:24 -0400

    Adds in example proto service and initial code for mcp go plugin for protoc

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"google.golang.org/protobuf/types/pluginpb"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+const version = "0.0.1"
+
+func main() {
+	showVersion := flag.Bool("version", false, "print the version and exit")
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("protoc-gen-mcp-go %v\n", version)
+		return
+	}
+
+	var flags flag.FlagSet
+
+	protogen.Options{
+		ParamFunc: flags.Set,
+	}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL) | uint64(pluginpb.CodeGeneratorResponse_FEATURE_SUPPORTS_EDITIONS)
+		gen.SupportedEditionsMinimum = descriptorpb.Edition_EDITION_PROTO2
+		gen.SupportedEditionsMaximum = descriptorpb.Edition_EDITION_2023
+		for _, f := range gen.Files {
+			if !f.Generate {
+				continue
+			}
+			generateFile(gen, f)
+		}
+		return nil
+	})
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module protoc-gen-mcp-go
+
+go 1.24
+
+require google.golang.org/protobuf v1.36.6
